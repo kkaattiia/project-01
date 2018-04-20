9cdcd1587dfc07f15adb4ff75bb756c91c2b1685
kkaattiia <36467921+kkaattiia@users.noreply.github.com>
2018-04-20 12:30:42 +0300

    js
    правки в css .page_head-sticky

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,21 +1,20 @@
 (function() {
- 
-  var pageHeader = document.querySelector(".page_header");
-  var pageHeaderHeight = pageHeader.getBoundingClientRect().bottom + window.pageYOffset;
-  
-  window.addEventListener("scroll", function() {
     
-    if (pageHeader.classList.contains("page_header-sticky") && (window.pageYOffset < pageHeaderHeight)) {
-        
-     pageHeader.classList.remove("page_header-sticky");
-       
-    } else if (window.pageYOffset > pageHeaderHeight) {
-        
-      pageHeader.classList.add("page_header-sticky");
-        
-   }
+    var pageHeader = document.querySelector(".page_header");
+    var pageHeaderHeight = pageHeader.getBoundingClientRect().bottom + window.pageYOffset;
     
-  }, false);
-  
-})(); 
-
+    window.addEventListener("scroll", function() {
+        
+     if (pageHeader.classList.contains("page_header-sticky") && (window.pageYOffset < pageHeaderHeight)) {
+         
+         pageHeader.classList.remove("page-header-sticky");
+         
+     } else if (window.pageYOffset > pageHeaderHeight) {
+         
+         pageHeader.classList.add("page_header-sticky");
+         
+     }
+        
+    }, false);
+    
+})()
