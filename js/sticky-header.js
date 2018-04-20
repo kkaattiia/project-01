dbb7efdd1a4a34fb973f8b66e6b398c028318d0f
kkaattiia <36467921+kkaattiia@users.noreply.github.com>
2018-04-20 12:02:49 +0300

    js
    добавлен script no-js, sticky-header

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,21 +1,21 @@
 (function() {
-+  
-+  var pageHeader = document.querySelector(".page_header");
-+  var pageHeaderHeight = pageHeader.getBoundingClientRect().bottom + window.pageYOffset;
-+  
-+  window.addEventListener("scroll", function() {
-+    
-+    if (pageHeader.classList.contains("page_header-sticky") && (window.pageYOffset < pageHeaderHeight)) {
-+        
-+      pageHeader.classList.remove("page_header-sticky");
-+        
-+    } else if (window.pageYOffset > pageHeaderHeight) {
-+        
-+      pageHeader.classList.add("page_header-sticky");
-+        
-+    }
-+    
-+  }, false);
-+  
-+})(); 
+ 
+  var pageHeader = document.querySelector(".page_header");
+  var pageHeaderHeight = pageHeader.getBoundingClientRect().bottom + window.pageYOffset;
+  
+  window.addEventListener("scroll", function() {
+    
+    if (pageHeader.classList.contains("page_header-sticky") && (window.pageYOffset < pageHeaderHeight)) {
+        
+     pageHeader.classList.remove("page_header-sticky");
+       
+    } else if (window.pageYOffset > pageHeaderHeight) {
+        
+      pageHeader.classList.add("page_header-sticky");
+        
+   }
+    
+  }, false);
+  
+})(); 
 
