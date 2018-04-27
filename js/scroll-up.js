d9e439c6f0ccb5a2f99da8625d7f4ad559e6bd4d
kkaattiia <36467921+kkaattiia@users.noreply.github.com>
2018-04-27 18:03:39 +0300

    js
    переписала scroll-up. js для плавного подьема

--- js/scroll-up.js
+++ js/scroll-up.js
@@ -1,25 +1,4 @@
-/*
-(function() {
-  
-  var scrollup = document.querySelector(".scroll-up");
-    
-  window.addEventListener("scroll", function() {
-    
-    if (window.pageYOffset < 300) {
-        
-      scrollup.classList.add("display-none");
-        
-    } else if (window.pageYOffset > 300) {
-        
-      scrollup.classList.remove("display-none");
-        
-    }
-    
-  }, false);
-  
-})(); */
-
-(function() {
+/*(function() {
     
     var scrollup = document.querySelector(".scroll-up");
     
@@ -36,4 +15,55 @@
         }
     }, false);
     
+})(); */
+
+(function(){
+  
+  var screenHeight = document.documentElement.clientHeight;
+  var btnToUp = document.querySelector(".toup");
+  var scrollHeight;
+ var timer;
+  
+function showBtn(){
+    
+  var scrollHeight = window.pageYOffset || document.documentElement.scrollTop;
+  if ((scrollHeight > screenHeight) && !btnToUp.classList.contains("toup-visible")) {
+        
+  btnToUp.classList.add("toup-visible");
+        
+} else if ((scrollHeight < screenHeight) && btnToUp.classList.contains("toup-visible")) {
+        
+  btnToUp.classList.remove("toup-visible");
+        
+}
+    
+  }
+  
+function scrollPage() {
+    
+  if (scrollHeight > 25) {
+        
+  window.scrollTo(0, scrollHeight);
+  scrollHeight = scrollHeight - 25;
+  timer = setTimeout(scrollPage, 10);
+        
+ } else {
+      
+ clearTimeout(timer);
+  window.scrollTo(0, 0);
+      
+ }
+    
+  }
+  
+  window.addEventListener("scroll", showBtn);
+  btnToUp.addEventListener("click", function(event) {
+    
+  event = event || window.event;
+  event.preventDefault();
+  scrollHeight = window.pageYOffset || document.documentElement.scrollTop;
+  scrollPage();
+    
+  });
+  
 })();
