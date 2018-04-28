b4a69db4fc5dc5da496f5a6419ccce79ed77de48
kkaattiia <36467921+kkaattiia@users.noreply.github.com>
2018-04-28 14:00:56 +0300

    js
    вернула старый header-sticky

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,18 +1,20 @@
 (function() {
-    
-    var scrollup = document.querySelector(".scroll-up");
-    
-    window.addEventListener("scroll", function() {
+     
+    var pageHeader = document.querySelector(".page_header");
+    var pageHeaderHeight = pageHeader.getBoundingClientRect().bottom + window.pageYOffset;
+     
+     window.addEventListener("scroll", function() {
+         
+     if (pageHeader.classList.contains("page_header-sticky") && (window.pageYOffset < pageHeaderHeight)) {
+         
+         pageHeader.classList.remove("page_header-sticky");
+         
+     } else if (window.pageYOffset > pageHeaderHeight) {
+         
+         pageHeader.classList.add("page_header-sticky");
+         
+     } 
         
-        if (window.pageYOffset < 300) {
-            
-            scrollup.classList.add("display-none");
-            
-        } else if (window.pageYOffset > 300) {
-            
-            scrollup.classList.remove("display-none");
-            
-        }
-    }, false);
-    
-})();
+     }, false);
+     
+})(); 
